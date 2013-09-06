e25d39f687a70ca6866d662753c072a2904af6d2
lygn128 <lygn128@163.com>
2013-09-06 09:07:12 +0800

    关于我们的导航栏和版本号

--- Classes/Controller/People/FSAboutViewController.m
+++ Classes/Controller/People/FSAboutViewController.m
@@ -39,41 +39,15 @@
     [_fsAboutContaierView release];
     [super dealloc];
 }
+-(NSString *)setTitle{
+    //_fsALLSettingContainerView.flag = 1;
+    return @"关于我们";
+}
 
 -(void)loadChildView{
-    
+    [super loadChildView];
     _fsAboutContaierView = [[FSAboutContaierView alloc] init];
     [self.view addSubview:_fsAboutContaierView];
-    /*
-    _navTopBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.view.frame.size.width, FSSETTING_VIEW_NAVBAR_HEIGHT)];
-#ifdef __IPHONE_5_0
-    [_navTopBar setBackgroundImage:[UIImage imageNamed: @"navigatorBar.png"] forBarMetrics:UIBarMetricsDefault];
-#endif
-	UINavigationItem *topItem = [[UINavigationItem alloc] init];
-	NSArray *items = [[NSArray alloc] initWithObjects:topItem, nil];
-	_navTopBar.items = items;
-	_navTopBar.topItem.title = NSLocalizedString(@"关于我们", nil);
-	[topItem release];
-	[items release];
-	[self.view addSubview:_navTopBar];
-    */
-    self.title = NSLocalizedString(@"关于我们", nil);
-    
-    
-    UIButton *returnBT = [[UIButton alloc] init];
-    [returnBT setBackgroundImage:[UIImage imageNamed:@"returnbackBT.png"] forState:UIControlStateNormal];
-    //[returnBT setTitle:NSLocalizedString(@"返回", nil) forState:UIControlStateNormal];
-    returnBT.titleLabel.font = [UIFont systemFontOfSize:12];
-    [returnBT addTarget:self action:@selector(returnBack:) forControlEvents:UIControlEventTouchUpInside];
-    [returnBT setTitleColor:COLOR_NEWSLIST_TITLE_WHITE forState:UIControlStateNormal];
-    returnBT.frame = CGRectMake(0, 0, 55, 30);
-    
-    UIBarButtonItem *returnButton = [[UIBarButtonItem alloc] initWithCustomView:returnBT];
-    self.navigationItem.leftBarButtonItem = returnButton;
-    [returnButton release];
-    [returnBT release];
-    
-      
 }
 
 -(void)returnBack:(id)sender{
@@ -82,25 +56,8 @@
 
 -(void)layoutControllerViewWithRect:(CGRect)rect{
     _fsAboutContaierView.backgroundColor = [UIColor whiteColor];
-    _fsAboutContaierView.frame = CGRectMake(0, FSSETTING_VIEW_NAVBAR_HEIGHT, rect.size.width, rect.size.height -FSSETTING_VIEW_NAVBAR_HEIGHT);
+    _fsAboutContaierView.frame = CGRectMake(0, 44, rect.size.width, rect.size.height -FSSETTING_VIEW_NAVBAR_HEIGHT);
 }
-
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView
-{
-}
-*/
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
-*/
-
 - (void)viewDidUnload
 {
     [super viewDidUnload];
